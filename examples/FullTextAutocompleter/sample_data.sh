3caf2e5b4d020263b505a3f07679297c61aeca85
Mukul Gupta <mukulgupta2507@gmail.com>
2014-07-13 20:10:47 +0530

    Create sample_data.sh
    File contains sample data from a mongodb collection which is used for indexing and searching purpose.

---
+++ examples/FullTextAutocompleter/sample_data.sh
@@ -0,0 +1,35 @@
+{
+    "_id": ObjectId("53c2890368103fc09aa86632"),
+    "title": "ElasticSearch The Definitve Guide",
+    "isbn": "9781449358549",
+    "author": "Clinton Gormley",
+    "category": "Technology",
+    "rating": 4
+}
+
+{
+    "_id": ObjectId("53c2896168103fc09aa86633"),
+    "title": "A Christmas Carol",
+    "isbn": "754628293",
+    "author": "Charles Dickens",
+    "category": "fiction",
+    "rating": 5
+}
+
+{
+    "_id": ObjectId("53c2899368103fc09aa86634"),
+    "title": "Mastering ElasticSearch",
+    "isbn": "754628212",
+    "author": "Marek Rogozinski",
+    "category": "Technology",
+    "rating": 4
+}
+
+{
+    "_id": ObjectId("53c289e768103fc09aa86635"),
+    "title": "Elasticsearch in Action",
+    "isbn": "9781617291",
+    "author": "Radu Gheorghe",
+    "category": "Technology",
+    "rating": 5
+}
